04d572c87cb72810bb75cd40fad80d239ce9ee24
sayedMabdullah <sayedmabdullah13@gmail.com>
2024-03-29 11:38:16 +0600

    D-2R747

---
+++ 800/A_Consecutive_Sum_Riddle.cpp
@@ -0,0 +1,46 @@
+/**
+ * Title: sayedMabdullah
+ * Date: "29-03-2024" Time:"11:28:37"
+ * do you have any query ?? sayedmabdullah13@gmail.com
+ *
+ * ---I'm starting in the name of Almighty ALLAH---
+ **/
+// Codeforces Round 747 (Div. 2)
+
+#include <bits/stdc++.h>
+#define ll long long
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define endl "\n"
+#define out cout
+#define in cin
+#define loop for (int i = 0; i < n; i++)
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    cout << -(n - 1) << ' ' << n << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    t = 1;
+    in >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+
+    /* Alhamdulillah For Everything */
+}
